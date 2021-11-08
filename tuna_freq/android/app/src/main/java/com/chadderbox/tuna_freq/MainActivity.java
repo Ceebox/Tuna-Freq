a8599afbdcbf2183f2cbac5e5379c48e1bacb426
Chadderbox <josephchadwick1309@gmail.com>
2021-11-08 17:00:26 +0000

    first commit

---
+++ tuna_freq/android/app/src/main/java/com/chadderbox/tuna_freq/MainActivity.java
@@ -0,0 +1,6 @@
+package com.chadderbox.tuna_freq;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ tuna_freq/lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fft/flutter_fft.dart';
+
+void main() => runApp(Application());
+
+class Application extends StatefulWidget {
+  @override
+  ApplicationState createState() => ApplicationState();
+}
+
+class ApplicationState extends State<Application> {
+  double? frequency;
+  String? note;
+  int? octave;
+  bool? isRecording;
+
+  FlutterFft flutterFft = FlutterFft();
+
+  _initialize() async {
+    // Keep asking for mic permission until it is accepted
+    while (!(await flutterFft.checkPermission())) {
+      flutterFft.requestPermission();
+    }
+
+    // await flutterFft.checkPermissions();
+    await flutterFft.startRecorder();
+    setState(() => isRecording = flutterFft.getIsRecording);
+
+    flutterFft.onRecorderStateChanged.listen(
+            (data) => {
+          setState(
+                () => {
+              frequency = data[1] as double,
+              note = data[2] as String,
+              octave = data[5] as int,
+            },
+          ),
+          flutterFft.setNote = note!,
+          flutterFft.setFrequency = frequency!,
+          flutterFft.setOctave = octave!,
+        },
+        onError: (err) {
+          print("Error: $err");
+        },
+        onDone: () => {print("Done")});
+  }
+
+  @override
+  void initState() {
+    isRecording = flutterFft.getIsRecording;
+    frequency = flutterFft.getFrequency;
+    note = flutterFft.getNote;
+    octave = flutterFft.getOctave;
+
+    flutterFft.setSubscriptionDuration = 0.1;
+
+    super.initState();
+    _initialize();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color gray = const Color(0xFF1A1A1A);
+    return MaterialApp(
+        title: "Simple flutter fft example",
+        theme: ThemeData.dark(),
+        color: Colors.blue,
+        home: Scaffold(
+          backgroundColor: gray,
+          body: Center(
+            child:
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                    ),
+                    child: isRecording!
+                        ? Text("Current note: ${note!} : ${octave!.toString()}",
+                        style: TextStyle(fontSize: 30, color: gray))
+                        : Text("Not Recording", style: TextStyle(fontSize: 35, color: gray)),
+                  ),
+                  Container(
+                      decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                      child: isRecording!
+                          ? Text(
+                          "Current frequency: ${frequency!.toStringAsFixed(2)}",
+                          style: TextStyle(fontSize: 30, color: gray))
+                          : Text("Not Recording", style: TextStyle(fontSize: 35, color: gray))
+                  )
+                ].map((e) => Padding(padding: const EdgeInsets.all(10), child: e)).toList(),
+              )
+          ),
+        ));
+  }
+}
